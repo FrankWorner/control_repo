074cde11df99c2eb799296485ba1790e2eaec950
FrankWorner <49485086+FrankWorner@users.noreply.github.com>
2019-06-26 15:31:53 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDCe9lj+oUmxMAZWgcf5YUhNBP9ROYu6vo2MpOGuJfeimfBB2kjjNaGuAyr9RWO980189g+YFUDRN6GRet3aMTijWooIECPjHUUllfpiqOEh+79QjDdZjJOZp6A9El2Kbx6l++6RKLM2F4UfIl/IFJAWGOYJGIblhWqWxwQ9NXA92PFW6RuXIGY8L7oQCLbr/OOy8O5ak7C1aeARCcFH5Pl/W6DO2tQRWu8t6vP5jEE7CsBq7lQHjHoGT9eJ4GwimAoDYG4pmNg+sTnpZ92jxzZI6hLXioS5ELA6e3b/+89pZsq/33tMYDjgtie1zwkTfBHOS6OlEk4OLCpy1h/GNb/ root@master.puppet.vm',
 	}  
 }
